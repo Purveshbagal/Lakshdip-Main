40ea0bc7fb62db3ef0ed1b6bf3cd678c025a8870
Admin <admin@example.com>
2026-06-10 09:36:53 +0530

    Update project files

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-
 declare global {
   // eslint-disable-next-line no-var
   var _mongoose: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null } | undefined
